5ede331a1b93c200de6da5fe0696fe63e7ed3503
Justin Silvestre <justinsilvestre@gmail.com>
2015-03-27 00:37:32 -0500

    heroku tweak

--- db/migrate/20150325035430_create_attendances.rb
+++ db/migrate/20150325035430_create_attendances.rb
@@ -6,7 +6,7 @@ class CreateAttendances < ActiveRecord::Migration
 
       t.timestamps null: false
     end
-    add_foreign_key :attendances, :attendees
-    add_foreign_key :attendances, :attended_events
+    add_foreign_key :attendances, :users, :attendee_id
+    add_foreign_key :attendances, :events, :attended_event_id
   end
 end
